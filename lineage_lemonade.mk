91d9ff856843dcf67e2ac5c291587884dabd0029
TechPanelGM <giomook2003@gmail.com>
2026-02-21 18:44:27 +0000

    lemonade: Switch to mini gapps

--- lineage_lemonade.mk
+++ lineage_lemonade.mk
@@ -15,6 +15,8 @@ $(call inherit-product, device/oneplus/lemonade/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+TARGET_USES_MINI_GAPPS := true
+
 PRODUCT_NAME := lineage_lemonade
 PRODUCT_DEVICE := lemonade
 PRODUCT_MANUFACTURER := OnePlus
